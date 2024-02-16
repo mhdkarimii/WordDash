39081056fcf1036ec674672405a297750e90cf1f
Mahdi Karimi <mhdkarimi2002@gmail.com>
2024-02-16 15:35:13 +0330

    added client side

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 const express = require('express');
+const path = require('path')
 require('./db/mongoose');
 const userRouter = require('./routers/user')
 
 const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
 const port = process.env.PORT || 3000
+
+app.use(express.static(publicDirectoryPath))
 
 app.use(express.json())
 app.use(userRouter)
 
-
+app.get('/', (req, res) => {
+    res.sendFile(path.join(publicDirectoryPath));
+})
 
  
 app.listen(port, () => {
