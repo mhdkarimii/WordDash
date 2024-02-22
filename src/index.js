2bf9b0fc0bd95f631b7fd7628dc530be17366b82
Mahdi Karimi <mhdkarimi2002@gmail.com>
2024-02-22 12:24:37 +0330

    middleware

--- src/index.js
+++ src/index.js
@@ -9,6 +9,21 @@ const port = process.env.PORT || 3000
 
 app.use(express.static(publicDirectoryPath))
 
+
+// app.use((req, res, next) => {
+//     if (req.method === 'GET') {
+//         res.send('GET requests are disabled')
+//     } else {
+//         next()
+//     }
+
+// })
+
+// app.use((req, res, next) => {
+//     res.status(503).send("Under maintenance")
+// })
+
+
 app.use(express.json())
 app.use(userRouter)
 
---
+++ src/middleware/auth.js
@@ -0,0 +1,8 @@
+const auth = async (req, res, next) => {
+    console.log('Auth Middleware')
+    next()
+}
+
+
+
+module.exports = auth
