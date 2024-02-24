274e9a73e6d5e7c467dc47c7743204f3b7591b0b
Mahdi Karimi <mhdkarimi2002@gmail.com>
2024-02-24 15:38:04 +0330

    logout route

--- src/routers/user.js
+++ src/routers/user.js
@@ -34,6 +34,21 @@ router.post('/users/login', async (req, res) => {
     }
 })
 
+
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+
+        await req.user.save()
+
+        res.send()
+    } catch(error) {
+        res.status(500).res.send()
+    }
+})
+
 router.get('/users/me', auth, async (req, res) => {
     res.send(req.user)
 })
